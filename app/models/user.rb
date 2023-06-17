662a5e27ecb0a740dcad86f06d0ea72d5c7762a0
TECHkoumu <nariyuki08@gmail.com>
2023-06-17 11:48:02 +0900

    ビューファイルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to :department, default: -> { Department.find_by(id: 1) }
+  # belongs_to :position, default: -> { Position.find_by(id: 1) }
+       
+  validates :name, presence: true
+  validates :department_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :position_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+       
+  has_many :manuals
+  has_many :comments
 end
